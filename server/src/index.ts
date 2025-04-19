0641877555577c5fad4a649fbbb72e9a70e8ebb1
pixelPicker <rj568614gmail.com>
2025-04-19 16:02:16 +0530

    removed supertokens

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import logger from './config/logger';
 import express from "express";
-import supertokens from "supertokens-node";
-import { middleware } from 'supertokens-node/framework/express';
 import cors from "cors";
 import { pinoHttp } from 'pino-http';
+import "../src/config/supertokens.js";
+
 
 const app = express();
 
@@ -15,12 +15,10 @@ app.use(pinoHttp({logger}));
 // cors init
 app.use(cors({
   origin: "http://localhost:5173",
-  allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+  allowedHeaders: ["content-type"],
   credentials: true,
 }))
 
-// supertokens middleware init
-app.use(middleware());
 
 app.get("/", (req, res) => {
   res.status(200).end("Welcome to qrbites. Have a great day");
